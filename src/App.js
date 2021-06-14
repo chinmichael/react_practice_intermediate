e1dd79f89ee40899ce71ef2712104d7534233d6e
chinmichael <chinmichael@naver.com>
2021-06-14 19:53:50 +0900

    react basic 5: react-bootstrap, etc

--- src/App.js
+++ src/App.js
@@ -1,24 +1,121 @@
-import logo from './logo.svg';
+/* eslint-disable */
+
 import './App.css';
+import React from 'react';
+import { useState } from 'react';
+
+import { Navbar, Nav, NavDropdown, Jumbotron, Button, Container, Row, Col } from 'react-bootstrap'
+import { product } from './ref/data.js'
+import { countBy } from 'lodash';
 
 function App() {
+
+  let [product_data, product_change] = useState(product);
+
+  function product_list() {
+    let list = [];
+    list.length = product_data.length;
+
+    for (let i = 0; i < product_data.length; i++) {
+      let imgUrl = `https://codingapple1.github.io/shop/shoes${i + 1}.jpg`;
+
+      list[i] =
+        <Col md={4} key={i}>
+          <img src={imgUrl} width="100%"></img>
+          <h4>{product_data[i].title}</h4>
+          <p>{product_data[i].content} & {product_data[i].price}</p>
+        </Col >
+    }
+
+    return list;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Navbar bg="dark" expand="lg" variant="dark">
+        <Navbar.Brand href="#home">React Ex</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <Jumbotron className="background" style={{ textAlign: 'center', }}>
+        <h1>20% Season off</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a simple hero unit, a simple jumbotron-style component for calling
+          extra attention to featured content or information.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </Jumbotron>
+
+      <Container style={{ textAlign: 'center', }}>
+        <Row>
+
+          {/* {product_list()} */}
+          {/* 실행을 위해 ()을 붙이는거 유의 아래는 map으로 처리한 경우 */}
+
+          {/* {product.map((pro_data, i) => {
+            return (
+              <Col md={4}>
+                <img src={`https://codingapple1.github.io/shop/shoes${i + 1}.jpg`} width="100%"></img>
+                <h4>{pro_data.title}</h4>
+                <p>{pro_data.content} & {pro_data.price}</p>
+              </Col>
+            )
+          })} */}
+
+          {/* 최종적으로 map(혹은 for문으로 해도 됨 map이 더 직관적) + props + 컴포넌트 이용 */}
+          {
+            product.map((pro_list, i) => {
+              return (
+                <ProductList pro_list={pro_list} cnt={i + 1} key={i}></ProductList>
+              )
+            })
+          }
+
+
+          {/* <Col md={4}>
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%"></img>
+            <h4></h4>
+            <p></p>
+          </Col>
+          <Col md={4}>
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%"></img>
+            <h4></h4>
+            <p></p>
+          </Col>
+          <Col md={4}>
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%"></img>
+            <h4></h4>
+            <p></p>
+          </Col> */}
+          {/* 오랜만의 보는 부트스트랩 기본 반응형 문법 : container랑 row로 감싸고 col을 총 12가 되게 나눔 (md, lg등은 기준) */}
+        </Row>
+      </Container>
+    </div >
+  );
+}
+
+function ProductList(props) {
+  return (
+    <Col md={4}>
+      <img src={`https://codingapple1.github.io/shop/shoes${props.cnt}.jpg`} width="100%"></img>
+      <h4>{props.pro_list.title}</h4>
+      <p>{props.pro_list.content} & {props.pro_list.price}</p>
+    </Col>
   );
 }
 
